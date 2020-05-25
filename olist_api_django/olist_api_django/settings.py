ce1696951353012f40ed5ae5226d6e188843b251
Davi <davi.wl@gmail.com>
2020-05-25 19:06:32 -0300

    allowed hosts for heroku

--- olist_api_django/olist_api_django/settings.py
+++ olist_api_django/olist_api_django/settings.py
@@ -28,7 +28,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['olist-api-django.herokuapp.com']
 
 
 # Application definition
